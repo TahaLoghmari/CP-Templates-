da4cb19cce6bf99224d0fb996442d0cc002e9b31
Mohamed Taha Loghmari <medtahalog@gmail.com>
2025-10-11 07:43:16 +0100

    Add missing includes and comments for order statistics tree

--- Data_structures/os.cpp
+++ Data_structures/os.cpp
@@ -1,3 +1,8 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 template<typename T, typename Cmp = less<T>>
 using iset = tree<T, null_type, Cmp, rb_tree_tag,
           tree_order_statistics_node_update, allocator<T>>;
+// order_of_key: returns to the number of items that are strictly smaller than our item k
+// find_by_order(k): returns to an iterator to the kth element (counting from zero) in the set
